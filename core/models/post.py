e72fe97c1e4598bdf9a826c8c417c06c6e5130da
Rasulov Shodiyor <108322250+ruymhdi@users.noreply.github.com>
2024-01-14 03:27:02 -0500

    created mixins

--- core/models/post.py
+++ core/models/post.py
@@ -1,4 +1,5 @@
 from .base import Base
+from .mixins import UserRelationMixin
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Text, ForeignKey
@@ -8,16 +9,14 @@ if TYPE_CHECKING:
     from .user import User
 
 
-class Post(Base):
+class Post(UserRelationMixin,Base):
+    # _user_id_nullable = False
+    # _user_id_unique = False
+    _user_back_populates = "posts"
+
     title: Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(
 	      Text, 
 		    default="",
 		    server_default="",
     )
-
-    user_id = Mapped[int] = mapped_column(
-          ForeignKey["user.id"],
-          # nullable=False,
-    )
-    user: Mapped["User"] = relationship(back_populates="posts")
--- core/models/profile.py
+++ core/models/profile.py
@@ -1,4 +1,5 @@
 from .base import Base
+from .mixins import UserRelationMixin
 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, ForeignKey
@@ -8,9 +9,9 @@ if TYPE_CHECKING:
 	from .user import User
 
 
-class Profile(Base):
+class Profile(UserRelationMixin, Base):
+	_user_id_unique = True
+	_user_back_populates = "profile"
 	first_name: Mapped[str | None] = mapped_column(String(40), unique=False)
 	last_name: Mapped[str | None] = mapped_column(String(40), unique=False)
 	bio: Mapped[str | None]
-	
-	user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), unique=True)
