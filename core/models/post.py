ef761cd48b6858c0267c0ed72936758013454b39
Rasulov Shodiyor <108322250+ruymhdi@users.noreply.github.com>
2024-01-14 02:31:23 -0500

    created post

--- core/models/post.py
+++ core/models/post.py
@@ -5,13 +5,14 @@ from sqlalchemy import String, Text, ForeignKey
 
 
 class Post(Base):
-	title: Mapped[str] = mapped_column(String(100), unique=False)
+    title: Mapped[str] = mapped_column(String(100), unique=False)
     body: Mapped[str] = mapped_column(
-		Text, 
-		default="",
-		server_default="",
+	      Text, 
+		    default="",
+		    server_default="",
     )
 
     user_id = Mapped[int] = mapped_column(
-          Foreignkey[users.id]
-    )    
+          ForeignKey["user.id"],
+          # nullable=False,
+    )
